9cf5dd34d77715d97a6dab14c7f91c942fb1cdd0
khanarezamaulana <khanarezamaulana@gmail.com>
2018-12-11 13:50:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import Card from './Card';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  state = { Content: "" }
+
+  componentDidMount() {
+    axios.get("https://safe-falls-22549.herokuapp.com/random_joke")
+    .then(
+      (joke) => {
+        this.setState({
+          Content: joke.data
+        })
+      }
+    )
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+        <div className="container text-right py-5">
+          <Card setup={this.state.Content.setup} punchline={this.state.Content.punchline} />
+        </div>
+      </React.Fragment>
+    )
   }
 }
-
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render() {
+        return(
+            <div className="card w-50 mx-auto" style={{ marginTop: "200px" }}>
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.setup}</h5>
+                    <p className="card-text font-weight-bold">{this.props.punchline} <i class="far fa-grin-squint-tears"></i></p>
+                    <a href="" className="btn btn-danger" onClick={() => { window.location.reload() }}>Reload <i class="fas fa-sync-alt"></i></a>
+                </div>
+            </div>
+        )
+    }
+}
+export default Card;
