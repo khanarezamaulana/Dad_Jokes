82591abc4598c2e2de060f3f21f5eeb0b9a3b628
khanarezamaulana <khanarezamaulana@gmail.com>
2018-12-11 13:59:01 +0700

    first commit

--- src/Card.js
+++ src/Card.js
@@ -6,8 +6,8 @@ class Card extends React.Component {
             <div className="card w-50 mx-auto" style={{ marginTop: "200px" }}>
                 <div className="card-body">
                     <h5 className="card-title">{this.props.setup}</h5>
-                    <p className="card-text font-weight-bold">{this.props.punchline} <i class="far fa-grin-squint-tears"></i></p>
-                    <a href="" className="btn btn-danger" onClick={() => { window.location.reload() }}>Reload <i class="fas fa-sync-alt"></i></a>
+                    <p className="card-text font-weight-bold">{this.props.punchline} <i class="fas fa-laugh-squint"></i></p>
+                    <a href="" className="btn btn-danger" onClick={() => { window.location.reload() }}>Reload <i class="fas fa-redo-alt"></i></a>
                 </div>
             </div>
         )
